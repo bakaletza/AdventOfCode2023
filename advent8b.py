f6dde6f3da4249c02c26ffbfe7aff00745a9d996
Amy Bakaletz <amy.bakaletz@knoxschools.org>
2023-12-09 13:04:34 -0500

    Day 8b - works on Test but still too slow even with left/right index tracking

--- advent8b.py
+++ advent8b.py
@@ -7,12 +7,16 @@ dir = data[0]
 
 nodes = []
 left = []
+leftIndex = []
 right = []
+rightIndex = []
 
 for i in range(2, len(data)):
     nodes.append(data[i][0:3])
     left.append(data[i][7:10])
+    leftIndex.append(-1)
     right.append(data[i][12:15])
+    rightIndex.append(-1)
 
 found = False
 nextDirIndex = 0
@@ -31,19 +35,35 @@ for i in range(len(nodes)):
 
 count = 0
 
+print(aNodes)
+print(zNodes)
 while not found:
     count += 1
     nextDir = dir[nextDirIndex]
+
     for i in range(len(aNodes)):
+        skipLoop = False
+        thisNode = aNodes[i]
         if(nextDir == "L"):
             nextNode = left[aNodes[i]]
+            if(leftIndex[aNodes[i]] != -1):
+                aNodes[i] = leftIndex[aNodes[i]]
+                skipLoop = True
         else:
             nextNode = right[aNodes[i]]
-        for j in range(len(nodes)):
-            if nodes[j] == nextNode:
-                aNodes[i] = j
-                break
-
+            if (rightIndex[aNodes[i]] != -1):
+                aNodes[i] = rightIndex[aNodes[i]]
+                skipLoop = True
+        if(not skipLoop):
+            for j in range(len(nodes)):
+                if nodes[j] == nextNode:
+                    aNodes[i] = j
+                    if(nextDir == "L"):
+                        leftIndex[thisNode] = j
+                    else:
+                        rightIndex[thisNode] = j
+                    break
+    #print(str(aNodes[0]) + " " + str(aNodes[1]))
     if(aNodes == zNodes):
         print(count)
         found = True
