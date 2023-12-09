98e1bd40b07fd29a0178c188660d0683ec215ce1
Amy Bakaletz <amy.bakaletz@knoxschools.org>
2023-12-09 07:33:41 -0500

    Day 8b - works on test data but not on real

---
+++ advent8b.py
@@ -0,0 +1,55 @@
+# read in the data file
+file = open("advent8.txt", "r")
+data = file.read().strip('\n\r').splitlines()
+file.close()
+
+dir = data[0]
+
+nodes = []
+left = []
+right = []
+
+for i in range(2, len(data)):
+    nodes.append(data[i][0:3])
+    left.append(data[i][7:10])
+    right.append(data[i][12:15])
+
+found = False
+nextDirIndex = 0
+
+# find nodes that end in A
+aNodes = []
+for i in range(len(nodes)):
+    if(nodes[i][2] == "A"):
+        aNodes.append(i)
+count = 0
+
+while not found:
+    count += 1
+    nextDir = dir[nextDirIndex]
+    for i in range(len(aNodes)):
+        if(nextDir == "L"):
+            nextNode = left[aNodes[i]]
+        else:
+            nextNode = right[aNodes[i]]
+        for j in range(len(nodes)):
+            if nodes[j] == nextNode:
+                aNodes[i] = j
+                break
+
+    zNode = True
+    for i in range(len(aNodes)):
+        if(nodes[aNodes[i]][2] != "Z"):
+            zNode = False
+    if zNode:
+        print(count)
+        found = True
+
+
+
+
+
+    else:
+        nextDirIndex += 1
+        if(nextDirIndex == len(dir)):
+            nextDirIndex = 0
