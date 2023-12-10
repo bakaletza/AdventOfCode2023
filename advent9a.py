79dde7cab3752e73c41553f81b0dee86cf0c9b1b
Amy Bakaletz <amy.bakaletz@knoxschools.org>
2023-12-09 19:12:11 -0500

    Day 9a

---
+++ advent9a.py
@@ -0,0 +1,34 @@
+# read in the data file
+file = open("advent9.txt", "r")
+data = file.read().strip('\n\r').splitlines()
+file.close()
+
+sum = 0
+for i in range(len(data)):
+    nums = []
+    nums.append([int(x) for x in data[i].split()])
+    allZeros = False
+    row = 0
+    while not allZeros:
+        allZeros = True
+        nums.append([])
+        row += 1
+        for j in range(1, len(nums[row-1])):
+            diff = nums[row-1][j] - nums[row-1][j-1]
+            nums[row].append(diff)
+            if(diff != 0):
+                allZeros = False
+    # extrapolate
+    nums[row].append(0)
+    row -= 1
+    while row >= 0:
+        value = nums[row+1][-1] + nums[row][-1]
+        nums[row].append(value)
+        if row == 0:
+            sum += value
+        row -= 1
+
+    print(nums)
+print(sum)
+
+
